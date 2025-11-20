055c5f929c118983576eafbbe6859d21ceae331a
usmansubla <eausman745@gmail.com>
2025-11-20 20:30:24 +0530

    error and response added

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import cookieParser from "cookie-parser"
+import express from "express"
+const app =express()
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    creadentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export {app}
